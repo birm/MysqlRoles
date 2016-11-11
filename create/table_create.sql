ee2079f8a90a85d7b2bd3fee160c45183b0ee676
Ryan Birmingham <birm@rbirm.us>
2016-11-11 12:10:59 -0500

    added sql for table creates

---
+++ create/table_create.sql
@@ -0,0 +1,85 @@
+CREATE TABLE user (
+  `FromHost` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `UserName` char(16) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `Plugin` char(64) COLLATE utf8_bin DEFAULT '',
+  `Authentication_String` text COLLATE utf8_bin,
+  PRIMARY KEY (`UserName`)
+);
+
+CREATE TABLE host (
+  `Name` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `Address` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `Comments` text COLLATE utf8_bin,
+  PRIMARY KEY (`Name`)
+);
+
+CREATE TABLE UserGroup (
+  `Name` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `Description` text COLLATE utf8_bin,
+  PRIMARY KEY (`Name`)
+);
+
+CREATE TABLE HostGroup (
+  `Name` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `Description` text COLLATE utf8_bin,
+  PRIMARY KEY (`Name`)
+);
+
+CREATE TABLE HostGroupMembership (
+  `HostName` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `GroupName` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  FOREIGN KEY (`HostName`) REFERENCES Host(Name),
+  FOREIGN KEY (`GroupName`) REFERENCES HostGroup(Name)
+);
+
+CREATE TABLE UserGroupMembership (
+  `UserName` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `GroupName` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  FOREIGN KEY (`UserName`) REFERENCES User(UserName),
+  FOREIGN KEY (`GroupName`) REFERENCES UserGroup(Name)
+);
+
+CREATE TABLE PermissionType (
+  `Name` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `Select_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Insert_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Update_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Delete_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Create_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Drop_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Reload_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Shutdown_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Process_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `File_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Grant_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `References_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Index_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Alter_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Show_db_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Super_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Create_tmp_table_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Lock_tables_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Execute_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Repl_slave_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Repl_client_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Create_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Show_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Create_routine_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Alter_routine_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Create_user_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Event_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Trigger_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  `Create_tablespace_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`Name`)
+);
+
+CREATE TABLE Access(
+  `Name` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `UserGroup` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `HostGroup` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `PermissionType` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`Name`),
+  FOREIGN KEY (`UserGroup`) REFERENCES UserGroup(Name),
+  FOREIGN KEY (`HostGroup`) REFERENCES HostGroup(Name),
+  FOREIGN KEY (`PermissionType`) REFERENCES PermissionType(Name)
+);
