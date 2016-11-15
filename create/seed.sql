e430115b9e83d60b6afeab063c84ba82873e5541
Ryan Birmingham <birm@rbirm.us>
2016-11-15 00:40:09 -0500

    add group memberships to seed

--- create/seed.sql
+++ create/seed.sql
@@ -20,14 +20,14 @@ insert into user_group (Name) values ("Testing");
 insert into user_group (Name) values ("Reporting");
 insert into user_group (Name) values ("Admin");
 
---host groups
+-- host groups
 insert into host_group (Name) values ("all");
 insert into host_group (Name) values ("dev_stack");
 insert into host_group (Name) values ("preprod");
 insert into host_group (Name) values ("prod");
 insert into host_group (Name) values ("report");
 
---permission types
+-- permission types
 insert into permission_type (Name, Select_priv, Show_db_priv) values ("read", "Y", "Y");
 insert into permission_type (Name, Select_priv, File_priv, Show_db_priv) values ("readfile", "Y", "Y", "Y");
 insert into permission_type (Name, Select_priv, Insert_priv, Update_priv, Delete_priv, Show_db_priv) values ("readwrite", "Y", "Y", "Y", "Y", "Y");
@@ -37,7 +37,23 @@ insert into permission_type (Name, Lock_tables_priv, Execute_priv, Repl_slave_pr
 ("admin", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y");
 insert into permission_type values ("ALL", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y");
 
---access grants
+-- user group memberships
+insert into user_group_membership values ("Alan", "Audit"), ("Danice", "Development"), ("Terry", "Testing"), ("Rachel", "Reporting"), ("Sam", "Admin");
+
+-- insert into host groups
+--  -- dev_stack
+insert into host_group_membership values ("staging", "dev_stack"), ("testing", "dev_stack"), ("cert", "dev_stack"), ("prod", "dev_stack");
+-- -- preprod
+insert into host_group_membership values ("staging", "preprod"), ("testing", "preprod"), ("cert", "preprod");
+-- -- prod
+insert into host_group_membership values ("prod", "prod");
+-- -- report
+insert into host_group_membership values ("report", "report"), ("ops", "report");
+-- -- all
+insert into host_group_membership values ("report", "all"), ("staging", "all"), ("testing", "all"), ("cert", "all"), ("prod", "all"), ("ops", "all");
+
+
+-- access grants
 insert into access values ("Audit", "Audit", "all", "read");
 insert into access values ("Development", "Development", "dev_stack", "schemachanges");
 insert into access values ("Testing", "Testing", "dev_stack", "readwrite");
