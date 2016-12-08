8d7a0720ece515d186bb7c495a91a30cfabbeb75
Ryan Birmingham <birm@rbirm.us>
2016-12-08 16:01:34 -0500

    Testing ready

--- MysqlRoles/tests/test_default_inits.py
+++
@@ -1,8 +0,0 @@
-from MysqlRoles import RoleServ
-from MysqlRoles import RoleManage
-
-def test_roleserv_init():
-    rs = RoleServ()
-
-def test_rolemanage_init():
-    rm = RoleManage("localhost")
--- MysqlRoles/tests/test_roleserv_fcns.py
+++ MysqlRoles/tests/test_roleserv_fcns.py
@@ -1,7 +1,9 @@
 from MysqlRoles import RoleServ
 
-rs=RoleServ()
+def test_basic_init():
+    rs = RoleServ()
 
 def test_create_tables():
+    rs = RoleServ()
     rs.create_tables()
     rs.test_seed_tables()
