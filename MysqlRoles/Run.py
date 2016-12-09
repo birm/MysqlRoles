5cc322f4539f7f4389d4fd1ff12df719a129eb57
Ryan Birmingham <birm@rbirm.us>
2016-12-09 10:59:14 -0500

    add __main__ and Run module

--- MysqlRoles/Run.py
+++ MysqlRoles/Run.py
@@ -1,15 +1,79 @@
 from MysqlRoles import RoleManage
 from MysqlRoles import RoleServ
+import pymysql
 
 
 class Run(object):
-  def parse(self):
-    """
-    Parse arguments given to CLI into commands to run.
-    """
-    # -- Desired Commands
-    # Run init (address/name for central server) -- creates empty tables
-    # Run seed (address/name for central server) -- seeds with test info
-    # Run update (address/name for central server) (address/name for host) -- updates the host as requested
-    pass
-    
+
+    @staticmethod
+    def parse(self, args):
+        """
+        Parse arguments given to CLI into commands to run.
+        """
+        # -- Desired Commands
+        # init (address/name for central server) -- creates empty tables
+        # seed (address/name for central server) -- seeds with test info
+        # update (address/name for central server) (address/name for host)
+        # -- updates the host as requested
+        helpstr = \
+        """Expected Command Line Usage of MysqlRoles:
+        init (address/name for central server)
+            creates empty tables
+        seed (address/name for central server)
+            seeds with test info
+        update (address/name for central server) (address/name for host)
+            updates the host as requested."""
+        if len(args) < 1:
+            print(helpstr)
+            return 1
+        elif args[0].lower() == "init":
+            if len(args) != 2:
+                print("init expects a hostname for a central server to init")
+                return 1
+            else:
+                cent = Run.net_test(args[1])
+                rs = RoleServ(cent)
+                rs.create_tables()
+                print "created tables on {0}".format(cent)
+                return 0
+        elif args[0].lower() == "seed":
+            if len(args) != 2:
+                print("seed expects a hostname for a central server to seed")
+                return 1
+            else:
+                cent = Run.net_test(args[1])
+                rs = RoleServ(cent)
+                rs.test_seed_tables()
+                print "test seeded on {0}".format(cent)
+                return 0
+        elif args[0].lower() == "update":
+            if len(args) != 3:
+                print("init expects a hostname for a central server to reference and a client to update")
+                return 1
+            else:
+                cent = Run.net_test(args[1])
+                client = Run.net_test(args[2])
+                rm = RoleServ(cent, client)
+                rm.update_users()
+                print "updated users on {0} from {1}".format(client, cent)
+                return 0
+        else:
+            print(helpstr)
+            return 1
+
+    @staticmethod
+    def net_test(self, host):
+        """
+        Determine if a host is accessible before doing anything.
+
+        Returns the host name back if the host is up and is able to connect with mysql.
+        To fix this issue, try:
+        - Update your my.cnf with credentials
+        - Start a mysql instance on the host
+        """
+        try:
+            pymysql.connect(host=host, db='mysql')
+            return host
+        except pymysql.err.OperationalError:
+            return False
+        pass
---
+++ MysqlRoles/__main__.py
@@ -0,0 +1,18 @@
+from MysqlRoles import Run
+import sys
+
+if __name__ == "__main__":
+    r = Run()
+    helpstr = \
+    """Expected Command Line Usage of MysqlRoles:
+    init (address/name for central server)
+        creates empty tables
+    seed (address/name for central server)
+        seeds with test info
+    update (address/name for central server) (address/name for host)
+        updates the host as requested."""
+    if len(sys.argv)<2:
+        print helpstr
+        return 1
+    s = r.parse(sys.argv[1:])
+    return 0
