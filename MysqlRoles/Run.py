bf828506fbd973f248745b77884488f011c1f595
Ryan Birmingham <birm@rbirm.us>
2016-12-09 03:03:07 -0500

    Create Run.py

---
+++ MysqlRoles/Run.py
@@ -0,0 +1,11 @@
+from MysqlRoles import RoleManage
+from MysqlRoles import RoleServ
+
+
+class Run(object):
+  def parse(self):
+  """
+  Parse arguments given to CLI into commands to run.
+  """
+    pass
+    
