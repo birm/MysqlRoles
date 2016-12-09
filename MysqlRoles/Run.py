3e4d7ad301fd7e32bc9bdf57d6d2822c48555a85
Ryan Birmingham <birm@rbirm.us>
2016-12-09 03:20:32 -0500

    Update Run.py

--- MysqlRoles/Run.py
+++ MysqlRoles/Run.py
@@ -7,5 +7,9 @@ class Run(object):
     """
     Parse arguments given to CLI into commands to run.
     """
+    # -- Desired Commands
+    # Run init (address/name for central server) -- creates empty tables
+    # Run seed (address/name for central server) -- seeds with test info
+    # Run update (address/name for central server) (address/name for host) -- updates the host as requested
     pass
     
