b14190d6cd86f45031957aac535502526e26eac9
Ryan Birmingham <birm@rbirm.us>
2016-12-12 12:46:15 -0500

    update to 1.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='MysqlRoles',
-      version='1.1.2',
+      version='1.2.0',
       description='Role Based Access Control (RBAC) for mysql',
       author='Ryan Birmingham',
       author_email='birm@rbirm.us',
