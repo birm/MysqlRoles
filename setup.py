c47385e9892e26c17ba1688de69000c3744c1504
Ryan Birmingham <birm@rbirm.us>
2016-12-14 10:04:28 -0500

    add flush stmt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='MysqlRoles',
-      version='1.2.5',
+      version='1.2.6',
       description='Role Based Access Control (RBAC) for mysql',
       author='Ryan Birmingham',
       author_email='birm@rbirm.us',
