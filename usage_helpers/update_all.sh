5a674058097c0172a37ba8d3c5fc8ae3b6ff4a8c
Ryan Birmingham <birm@rbirm.us>
2016-12-15 08:47:25 -0500

    Update update_all.sh

--- usage_helpers/update_all.sh
+++ usage_helpers/update_all.sh
@@ -5,5 +5,5 @@
 # to update all hosts managed by RBAC
 mysql -h $1 _MysqlRoles -ss -e "SELECT distinct(Address) from host;" | while read host; do
   echo Trying to update $host using _MysqlRoles schema on $1
-  python -m MysqlRoles $1 $host
+  python -m MysqlRoles update $1 $host
 done
